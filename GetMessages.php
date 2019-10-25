9947e0143acf746bfdc9b51fdfe9f9f257f6c4ec
Baris David Ekinci <davekinci@hotmail.com>
2019-10-25 19:44:02 +0200

    Get sample added

---
+++ GetMessages.php
@@ -0,0 +1,28 @@
+<?php 
+require __DIR__ . '/vendor/autoload.php';
+use Jumbojett\OpenIDConnectClient;
+
+$user = 'userName';
+$password = 'password';
+$from = '+17708002476';
+$to = '+10000000000';
+
+$url = 'http://sms.hostmycalls.com:3333/Messages';
+
+
+$oidc = new OpenIDConnectClient('https://hmcopenidserver.azurewebsites.net', $user,$password );
+
+$oidc->providerConfigParam(array('token_endpoint'=>'https://hmcopenidserver.azurewebsites.net/connect/token')); 
+$oidc->addScope('api1'); // name of api -> change it according to the API accessed
+
+$clientCredentialsToken = $oidc->requestClientCredentialsToken()->access_token;
+
+
+$authorization = "Authorization: Bearer "; 
+$authorization .= $clientCredentialsToken;
+$ch = curl_init($url);
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json',$authorization));
+
+$result = curl_exec($ch);
+?>
